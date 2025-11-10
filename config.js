6ca3ac76f0cf906b6bf9042751636b04e66e4e06
Văn Nhã <nha011246892@gmail.com>
2025-11-10 23:58:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,31 +3,21 @@ var config = {
         "Hà Nội 11.11.2025",      
         "Mỹ Hào, Hưng Yên 11.11.2005",  
         "Em yêu à... 💕Hôm nay là ngày đặc biệt của em đó 🎂",
-        "Anh muốn nói điều này từ lâu rồi... 🥺",
-        "Chúc em luôn xinh đẹp như bây giờ 🌸",
+        "Anh đã nhìn thấy 11:11 không biết bao nhiêu lần rồi...🥺",
+        "Và hôm nay ngày ấy đã đến",
+        "Ngày đặc biệt này, xin được:",
+         "Chúc em luôn xinh đẹp như bây giờ 🌸",
         "Và luôn mỉm cười thật tươi nhé 😊",
-        "Anh đã nhìn thấy 11:11 không biết bao nhiêu lần rồi",
         "Thậc may mắn khi có em bên cạnh 🌈",
         "Chúc mừng sinh nhật người con gái anh thương nhất 🎉",
-        "Yêu em nhèo lắm 💞",
-        "BichNgoc~~",
+        "Chúc em chân cứng đá mềm, nhiệm vụ nào cũng hoàn thành khó khăn nào cũng vượt qua",
+        "Yêu em nhèo lắm 💞 BichNgoc~~",
     ],
-    /**
-     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
-     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
-     * 例如
-     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
-     *
-     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
-     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
-     */
     imgs: {
         "心爱的小可爱": "./imgs/xiaokeai2.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
     },
-    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "Bắt đầu nào em",
+        turn_on: "Bắt đầu",
         play: "Âm nhạc sẽ dẫn em vào không khí hôm nay",
         bannar_coming: "Thêm một chút màu sắc cho ngày tươi đẹp này",
         balloons_flying: "Bong bóng lên trời",
